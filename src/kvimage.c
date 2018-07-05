c8e2bebb3222519b583c99ec2e2660479288ca6c
alsve <snowshoewolf@gmail.com>
2018-07-05 11:43:07 +0700

    Adding kvimage.c for acquiring image's features

--- src/kvimage.c
+++ src/kvimage.c
@@ -38,11 +38,11 @@ extracted information would be an array of feature from the object.
 
 static CvPoint koeva_image_flood_fill_seed(IplImage *image)
 {
-	int x, y;
+        int x, y;
 
-	x = 0, y = 0;
+        x = 0, y = 0;
 
-	return CvPoint(x, y);
+        return CvPoint(x, y);
 }
 
 /*
@@ -54,23 +54,23 @@ static CvPoint koeva_image_flood_fill_seed(IplImage *image)
  */
 static int koeva_image_get_logical_image(IplImage *dst, IplImage *src)
 {
-	ASSERT(dst->width == src->width, "Images width are not equal\n");
-	ASSERT(dst->height == src->height, "Images height are not equal\n");
+        ASSERT(dst->width == src->width, "Images width are not equal\n");
+        ASSERT(dst->height == src->height, "Images height are not equal\n");
 
-	cvCvtColor(src, dst, CV_RGB2GRAY);
-	cvSmooth(dst, dst, CV_GAUSSIAN, KOEVA_BLUR_KERNEL_SIZE, 
-			 KOEVA_BLUR_KERNEL_SIZE, KOEVA_BLUR_SIGMA, KOEVA_BLUR_SIGMA);
-	cvCanny(dst, dst, KOEVA_CANNY_LOW_THRESHOLD, KOEVA_CANNY_HIGH_THRESHOLD,
-			3, 0);
-	cvDilate(dst, dst, NULL, 1);
-	cvErode(dst, dst, NULL, 1);
-	cvFloodFill(dst, koeva_image_flood_fill_seed(dst), cvScalarAll(255), 
-				cvScalarAll(0), cvScalarAll(0), NULL, 4, NULL);
+        cvCvtColor(src, dst, CV_RGB2GRAY);
+        cvSmooth(dst, dst, CV_GAUSSIAN, KOEVA_BLUR_KERNEL_SIZE, 
+                 KOEVA_BLUR_KERNEL_SIZE, KOEVA_BLUR_SIGMA, KOEVA_BLUR_SIGMA);
+        cvCanny(dst, dst, KOEVA_CANNY_LOW_THRESHOLD, KOEVA_CANNY_HIGH_THRESHOLD,
+                3, 0);
+        cvDilate(dst, dst, NULL, 1);
+        cvErode(dst, dst, NULL, 1);
+        cvFloodFill(dst, koeva_image_flood_fill_seed(dst), cvScalarAll(255), 
+                    cvScalarAll(0), cvScalarAll(0), NULL, 4, NULL);
 
-	return 0;
-	
+        return 0;
+    
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -81,11 +81,11 @@ error:
  * Returns average value of red color or error code
  */
 static int koeva_image_color_average_red(IplImage *logical_image, 
-										 IplImage *src)
+                                         IplImage *src)
 {
 
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -96,11 +96,11 @@ error:
  * Returns average value of green color or error code
  */
 static int koeva_image_color_average_green(IplImage *logical_image, 
-										   IplImage *src)
+                                           IplImage *src)
 {
 
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -111,11 +111,11 @@ error:
  * Returns average value of blue color or error code
  */
 static int koeva_image_color_average_blue(IplImage *logical_image, 
-										  IplImage *src)
+                                          IplImage *src)
 {
 
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -128,7 +128,7 @@ static int koeva_image_area(IplImage *logical_image)
 {
 
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -141,7 +141,7 @@ static int koeva_image_edge(IplImage *logical_image)
 {
 
 error:
-	return -EPERM;
+        return -EPERM;
 }
 
 /*
@@ -152,19 +152,19 @@ error:
  */
 struct image_features * kv_image_extract_feature(IplImage *src)
 {
-	ASSERT(src, "%s : image source is NULL\n", __FUNCTION__);
-	IplImage *logical_image = cvCreateImage(cvSize(src->width, src->height),
-											src->depth, 1);
-	struct image_feature *ret = malloc(sizeof(*ret));
-	ASSERT_MEM(ret);
+        ASSERT(src, "%s : image source is NULL\n", __FUNCTION__);
+        IplImage *logical_image = cvCreateImage(cvSize(src->width, src->height),
+                                                src->depth, 1);
+        struct image_feature *ret = malloc(sizeof(*ret));
+        ASSERT_MEM(ret);
 
-	koeva_image_get_logical_image(logical_image, src);
-	ret->red = koeva_image_color_average_red(logical_image, src);
-	ret->green = koeva_image_color_average_green(logical_image, src);
-	ret->blue = koeva_image_color_average_green(logical_image, src);
-	ret->area = koeva_image_area(logical_image);
-	ret->edge = koeva_image_edge(logical_image);
+        koeva_image_get_logical_image(logical_image, src);
+        ret->red = koeva_image_color_average_red(logical_image, src);
+        ret->green = koeva_image_color_average_green(logical_image, src);
+        ret->blue = koeva_image_color_average_green(logical_image, src);
+        ret->area = koeva_image_area(logical_image);
+        ret->edge = koeva_image_edge(logical_image);
 
 error:
-	return NULL;
+        return NULL;
 }
